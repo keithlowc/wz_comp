79b66a63a344921e32a53fdc2573d0c2866b4c96
keithlowc <keithlowc@gmail.com>
2020-12-21 00:22:42 -0500

    debug true again

--- warzone_general/settings.py
+++ warzone_general/settings.py
@@ -28,11 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '2m05(*m&n!wald@k^-v*au43ervkw740%^yubp&+4(*+1ii(fw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
 if SERVER == 'True':
     DEBUG = False
-else:
-    DEBUG = True
-
 
 ALLOWED_HOSTS = ['*']
 
