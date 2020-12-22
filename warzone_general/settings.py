c52494370365f884c08251738416496c45211b1f
keithlowc <keithlowc@gmail.com>
2020-12-21 20:12:27 -0500

    one more try

--- warzone_general/settings.py
+++ warzone_general/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '2m05(*m&n!wald@k^-v*au43ervkw740%^yubp&+4(*+1ii(fw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get('SERVER', '') != 'False'
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 # Application definition
 
