bf1f5c159081a91208e138d7ab2cbdf348c18273
keithlowc <keithlowc@gmail.com>
2021-01-28 00:25:24 -0500

    adding kd verification

--- core/admin.py
+++ core/admin.py
@@ -165,6 +165,8 @@ class StaffCustomCompetitionAdmin(admin.ModelAdmin):
                 'competition_banner',
 
                 # Verification values
+                'cod_kd_minimum_per_player_verification',
+                'cod_kd_maximum_per_player_verification',
                 'cod_verification_total_games_played',
                 'cod_verification_total_time_played',
 
@@ -186,7 +188,6 @@ class StaffCustomCompetitionAdmin(admin.ModelAdmin):
         # Verification values
         'cod_kd_minimum_per_player_verification',
         'cod_kd_maximum_per_player_verification',
-
         'cod_verification_total_games_played',
         'cod_verification_total_time_played',
 
