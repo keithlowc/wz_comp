ad009d39ee0dfbc10a66ae24030289409397c803
keithlowc <keithlowc@gmail.com>
2021-04-26 00:26:17 -0400

    adding models and tables

--- leaderboards/models.py
+++ leaderboards/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Teams(models.Model):
+    record_modes = [
+        ('Solos','Solos'),
+        ('Duos','Duos'),
+        ('Trios','Trios'),
+        ('Squads','Squads'),
+    ]
+
+    team_name = models.CharField(max_length = 15)
+    kills = models.IntegerField()
+    record_mode = models.CharField(max_length = 6, choices = record_modes)
+
+    class Meta:
+        verbose_name = 'Team'
+        verbose_name_plural = 'Teams'
+
+    def __str__(self):
+        return str(self.team_name)
